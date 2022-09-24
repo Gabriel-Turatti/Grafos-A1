d670fa53c5b29f2e892ba0540869c0b1385d383d
gabi-fs <gabrielafdasilveira@gmail.com>
2022-09-24 12:50:20 -0300

    ciclo euleriano sem testar e sem print

--- CicloEuleriano.py
+++ CicloEuleriano.py
@@ -1,7 +1,7 @@
 from Grafo1 import Grafo, Nodo
 
 
-def Hierholzer(g:Grafo):
+def Hierholzer(g: Grafo):
     C = {}
     for e in g.arestas():
         C[frozenset(e[0].id(), e[1].id())] = False
@@ -16,15 +16,14 @@ def Hierholzer(g:Grafo):
     else:
         return (True, ciclo)
 
-        
 
-def buscaCiclo(g:Grafo, v:Nodo, C):
+def buscaCiclo(g: Grafo, v: Nodo, C):
     ciclo = []
     ciclo.append(v)
     t = v
     while True:
         u = None
-        for x in v.vizinhos().keys():
+        for x in list(v.vizinhos().keys()):
             i = 0
             if C[frozenset(x, v.id())] == False:
                 u = x
@@ -36,7 +35,23 @@ def buscaCiclo(g:Grafo, v:Nodo, C):
         ciclo.append(v)
         if v == t:
             break
+    for x in ciclo:
+        for u in list(x.vizinhos().keys()):
+            if C[frozenset(x, u.id())] == False:
+                (r, ciclo2) = buscaCiclo(g, x, C)
+                if r == False:
+                    return (False, None)
+                i = ciclo.index(x)
+                ciclo[i] = ciclo2.pop(0)
+                for nodo in ciclo2:
+                    i += 1
+                    ciclo.insert(i, nodo)
+    return (True, ciclo)
 
-    
 
-        
+'''grafo = Grafo()
+with open(r'D:\UFSC\Grafos\atividade1\Grafos-A1\facebook_santiago.net', 'r') as arquivo:
+    info = arquivo.read()
+    info = info.split('\n')
+    grafo.lerArquivo(info)
+print(Hierholzer(grafo))'''
